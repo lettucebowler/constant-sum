c5221d763cf564414b07a03fa6685be2b1e60dcb
Grant Montgomery <montg433@d.umn.edu>
2020-01-21 13:52:46 -0600

    initial commit

---
+++ constant.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python
+import os;
+import sys;
+import subprocess;
+
+class partition():
+    def _init_(self, base, numPart, part):
+        self.n = base
+        self.p = numPart
+        self.part = part
+
+def checkConstant():
+    print("Hello from checkConstant!")
