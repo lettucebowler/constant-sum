43c13a4fb44fca8828b48a33f2ae317503a8e3d5
Grant A Montgomery <montg433@d.umn.edu>
2020-01-25 19:09:31 -0600

    bloop

--- test.sh
+++ test.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
-for i in {2..4..2}
-do
-  ./constant.py -n $i >> data/$i&
-done
+  rm start end
+  date +%s%3N > start
+  ./constant.py -n 10 >> test10
+  date +%s%3N > end
+  cat start
+  cat end
+
