8cf2b8ac26b013b0715f2f6b1163f61fb4827b2d
Grant Montgomery <montg433@d.umn.edu>
2020-01-28 00:56:12 -0600

    GAAH

--- constant.py
+++ constant.py
@@ -49,79 +49,11 @@ def findPossibleSums(n, part):
 
 # Create a random constant-sum-partition from supplied partition
 def genConstantSumPartition(part, g):
-    total = 0
-    for x in part:
-        total += x
-    # print("Total : "+ str(total))
-    p = len(part)
-    numList = list(range(1, total + 1))
-    group = list()
     groupList = list()
-    if p % 2 == 0 and total % 4 == 2:
-        return groupList
-    # if g == 2:
-    #     numList.remove(1)
-    groupIndex = 0
-    for groupIndex in range(len(part)):
-        print("group : " + str(groupIndex) + " " + str(g))
-        print("half : " + str((total + g) // 2))
-        del group
-        group = list()
+    return groupList
 
-        if g == 2 and 1 in numList:
-            numList.remove(1)
-        if g % 2 == 0 and (total + g) // 2 in numList:
-            print("half off")
-            numList.remove((total + g) // 2)
 
-        if groupIndex < len(part) - 1:
-            print("if")
-            # if groupIndex == 0 or g == numList[0] * 2:
-            #     temp = numList[1]
-            # else:
-            #     temp = numList[0]
-            curIndex = 0
-            temp = numList[curIndex]
-            while total + g - temp not in numList:
-                curIndex += 1
-                if curIndex < len(numList):
-                    temp = numList[curIndex]
-                else:
-                    break
-            # else:
-            #     temp = numList[0]
-            print(str(numList) + " " + str(temp))
-            group.append(temp)
-            numList.remove(temp)
-            if temp >= g:
-                temp = total + g - temp
-            else:
-                temp = g - temp
-            group.append(temp)
-            numList.remove(temp)
 
-            if part[groupIndex] > 2:
-                for gh in range(part[groupIndex] // 2 - 1):
-                    cur = 0
-                    while total - numList[cur] not in numList:
-                        # print(str(numList) + " : " + str(total - numList[cur]))
-                        cur += 1
-                    temp = numList[cur]
-                    print(str(numList) + " : " + str(temp))
-                    group.append(temp)
-                    numList.remove(temp)
-                    group.append(total-temp)
-                    numList.remove(total-temp)
-        else:
-            if g == 2 and 1 not in numList:
-                numList.append(1)
-            if g % 2 == 0 and (total + g) // 2 not in numList:
-                numList.append(total + g // 2)
-            for namber in numList:
-                group.append(namber)
-        groupList.append(group)
-        groupIndex += 1
-    return(sorted(groupList))
 
 
 class csp():
