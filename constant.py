69f8dbf8f2f2f3dfbe04fe352fb3fb2a110d84ac
Grant Montgomery <montg433@d.umn.edu>
2020-01-27 22:38:55 -0600

    working on it

--- constant.py
+++ constant.py
@@ -5,6 +5,7 @@ import subprocess;
 import argparse
 import itertools
 import math
+import random
 
 # Parser to get base number for computations
 parser = argparse.ArgumentParser(description='A parser')
@@ -46,33 +47,24 @@ def findPossibleSums(n, part):
             sumList.append(g)
     return sorted(sumList)
 
-# def all_combs(seq, parts, indexes=None, res=[], cur=0):
-#     if indexes is None: # indexes to use for combinations
-#         indexes = range(len(seq))
-#
-#     if cur >= len(parts): # base case
-#         yield [[seq[i] for i in g] for g in res]
-#         return
-#
-#     for x in itertools.combinations(indexes, r=parts[cur]):
-#         set_x = set(x)
-#         new_indexes = [i for i in indexes if i not in set_x]
-#         for comb in all_combs(seq, parts, new_indexes, res=res + [x], cur=cur + 1):
-#             yield comb
-#
-# #Calculate number of groups of a partition.
-# def calcGroups(calcTop, calcBottom):
-#     tempCalc = math.factorial(calcTop)
-#     for h in calcBottom:
-#         tempCalc //= math.factorial(h)
-#     return tempCalc
-#
-# # Calculates the sum of each group in a partition, given a permutation.
-# def checkConstant(calcNum, calcGroup):
-#     sums = set()
-#     for a in calcGroup:
-#         sums.add(sum(a) % calcNum)
-#     return sums
+# Create a random constant-sum-partition from supplied partition
+def genConstantSumPartition(part, g):
+    total = 0
+    for x in part:
+        total += x
+    # print("Total : "+ str(total))
+    p = len(part)
+    numList = list(range(1, total + 1))
+    group = list()
+    groupList = list()
+    if p % 2 == 0 and total % 4 == 2:
+        return groupList
+    # if g == 2:
+    #     numList.remove(1)
+    for grouping in part:
+        
+    return(sorted(groupList))
+
 
 class csp():
     def __init__(self, base, partNum, sum, part, perm):
@@ -99,11 +91,11 @@ for part in partition(n):
 partList.sort(reverse=True, key=len)
 
 # Print partition and possible sums
-temp = list()
+sumList = list()
 count = 0
 for part in partList:
-    del temp
-    temp = findPossibleSums(n, part)
-    count += len(temp)
-    print("Partition : " + str(part) + " Possible g : " + str(temp))
-print(str(count))
+    del sumList
+    sumList = findPossibleSums(n, part)
+    print(str(part) + " : " + str(sumList))
+    for sum in sumList:
+        print(str(part) + " : " + str(list(genConstantSumPartition(part, sum))))
