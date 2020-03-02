50b31e15ef1ebc7f804caa30618fe893ca2ee871
Grant Montgomery <montg433@d.umn.edu>
2020-03-02 13:34:54 -0600

    changes

---
+++ run.sh
@@ -0,0 +1,3 @@
+echo $1
+time ./constant.py -n $1
+echo
--- test.sh
+++ test.sh
@@ -1,7 +1,10 @@
 #!/usr/bin/env bash
-mkdir data
-for i in {2..256..2}
+# mkdir data
+for i in {2..512..2}
 do
-rm data/$i
-./constant.py -n $i >> data/$i&
+# rm data/$i
+# echo $i&
+# time ./constant.py -n $i&
+./run.sh $i
+# ./constant.py -n $i >> data/$i&
 done
