372b7d185850b2a435cc7cf6f0cd35b637394a31
Grant Montgomery <montg433@d.umn.edu>
2020-03-05 01:36:35 -0600

    wook

--- pairs.py
+++ pairs.py
@@ -13,11 +13,16 @@ check = [5, 6, 7, 17, 18, 19]
 nums = [2, 3, 4, 8, 9, 10, 14, 15, 16, 20, 21, 22]
 # nums = [1, 2]
 nums.sort()
-for x in range(math.factorial(len(nums))):
-    nPerm = []
-    nooms = [g for g in nums]
-    # test the xth permutation of nums
-    for y in range(len(nums)):
-        q = x // math.factorial(len(nooms) - 1) % len(nooms)
-        nPerm.append(nooms.pop(q))
+nPerm = []
+nooms = [g for g in nums]
+# test the xth permutation of nums
+for y in range(len(nums)):
+    q = n // math.factorial(len(nooms) - 1) % len(nooms)
+    nPerm.append(nooms.pop(q))
+bad = False
+for z in range(0, len(nums), 2):
+    if nPerm[z] + nPerm[z + 1] != check[z // 2]:
+        bad = True
+        break
+if bad == False:
     print(str(nPerm))
--- test.sh
+++ test.sh
@@ -1,10 +1,11 @@
 #!/usr/bin/env bash
 mkdir data
-for i in {4..256..4}
+for i in {0..479001600..1}
 do
-rm data/$i
+# rm data/$i
 # echo $i&
 # time ./constant.py -n $i&
 # ./run.sh $i&
-./constant.py -n $i >> data/$i&
+# ./constant.py -n $i >> data/$i&
+./pairs.py -n $i&
 done
