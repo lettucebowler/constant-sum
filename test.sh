e229b81b7f51b884861f2f92fd043598c1a732d6
Grant Montgomery <montg433@d.umn.edu>
2020-03-08 23:22:22 -0500

    test.sh outputs to console instead of to files now

--- test.sh
+++ test.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
-mkdir data
+# mkdir data
 # for i in {0..479001600..1}
 for i in {0..64..4}
 do
 # rm data/$i
 # echo $i&
-./constant.py -n $i | grep duplicate
+./constant.py -n $i
 # ./run.sh $i&
 # ./constant.py -n $i >> data/$i&
 done
