dafc8c550c41758b9d27426b024aa7a22ffae6a1
Lettucebowler <lettucebowler@gmail.com>
2020-03-12 00:01:59 -0500

    uses addition and modulo now instead of range()

--- orders.py
+++ orders.py
@@ -9,9 +9,11 @@ from functools import reduce
 parser = argparse.ArgumentParser(description='A parser')
 parser.add_argument("-n", dest='number', default=2, type=int)
 parser.add_argument("-p", dest='partitions', default=2, type=int)
+parser.add_argument("-t", dest='sum', default=2, type=int)
 args = parser.parse_args()
 n = args.number
 p = args.partitions
+t = args.sum
 
 def findSums(n, p):
     return sorted(sum for sum in range(1, n) if sum * p % n == n // 2)
@@ -19,5 +21,17 @@ def findSums(n, p):
 def get_order(n, t):
     return n * t // gcd(n, t) // t
 
+def gen_orders(n, t):
+    order_t = [0]
+    i = t
+    while i != 0:
+        order_t.append(i)
+        i = (i + t) % n
+
+    orders = dict()
+    for i, num in enumerate(range(n // len(order_t))):
+        orders.update({i: [num + i for num in order_t]})
+    return orders
+
 sums = findSums(n, p)
-print("{}".format(get_order(n, sums[0])))
+print("{}".format(gen_orders(n, t)))
