b2c650c75523898b9a8cdead93b152b1fcd7f65a
Lettucebowler <lettucebowler@gmail.com>
2020-03-15 23:50:05 -0500

    I think I figured out how to generate odds from unused orders, but I need to impliment it. Put unused orders into a 2d array, and pairs reflected across the center sum to 0 mod n

--- orders.py
+++ orders.py
@@ -16,12 +16,13 @@ p = args.partitions
 t = args.sum
 
 class gSum():
-    def __init__(self, n, t, p, csp, zsp, odds):
+    def __init__(self, n, t, p, csp, zsp, orders, odds):
         self.n = n
         self.p = p
         self.t = t
         self.csp = csp
         self.zsp = zsp
+        self.orders = orders
         self.odds = odds
 
     def to_string(self):
@@ -61,39 +62,45 @@ def checkListForErrors(candidate, zandidate, total, t, o):
     checkList = reduce(operator.concat, candidate)
     if zandidate != []:
         checkList += reduce(operator.concat, zandidate)
-    
     if len(checkList) != total:
-        errors.append("duplicates")
-        
+        errors.append("duplicates")   
     if any(sum(k) % total != t for k in candidate):
         errors.append("sum")
-
     oddCount = [f for f in candidate if len(f) % 2 == 1]
     if len(oddCount) != o:
         errors.append("oddsoff")
-        
     return errors
 
 def getCSP(total, part, t, odds):
     if part == 1:
         return gSum(total, t, part, list(range(total)), [], 0)
-    o_num = 2 * p // get_order_t(n, t) - 1
-    left = get_order_list(n, t, t)
-    right = get_order_list(n, n - t, 0)  
-    pairs = [[lefty, righty] for lefty, righty in zip(left, right)][:get_order_t(n, t) // 2]
+    o_num = 2 * part // get_order_t(n, t) - 1
+    rights = get_order_list(n, t, t)[:get_order_t(n, t) // 2]
+    lefts = get_order_list(n, n - t, 0)[:get_order_t(n, t) // 2] 
+    pairs = [[lefty, righty] for lefty, righty in zip(lefts, rights)]
     for offset in range(1, o_num // 2 + 1):
-        left = get_order_list(n, t, offset)
-        right = get_order_list(n, n - t, (-1 * offset) % t)
-        pairs += [[lefty, righty] for lefty, righty in zip(left, right)]
+        rights = get_order_list(n, t, offset)
+        lefts = get_order_list(n, n - t, (-1 * offset) % t)
+        pairs.extend([[lefty, righty] for lefty, righty in zip(lefts, rights)])
+    unused_orders = [v for v in range(1, t) if v >= o_num + 1]
     leftovers = sorted({tuple(sorted((x, total - x))) for x in range(total) \
         if not any(x in s for s in pairs)})
     pairs += checkListForErrors(pairs, leftovers, total, t, odds)
-    return gSum(total, t, part, pairs, leftovers, 0)
+    return gSum(total, t, part, pairs, leftovers, unused_orders, 0)
+
+def getOdds(const):
+    withOdds = []
+    unused_orders = [get_order_list(n, t, d) for d in const.orders]
+    print("{}".format(unused_orders))
+     
+    return withOdds
 
 
 sums = findSums(n, p)
 # print("{}".format(gen_orders(n, sums[0])))
-for d in range(0, n // get_order_t(n, t)):
-    print("{}".format(get_order_list(n, t, d)))
+# for d in range(0, n // get_order_t(n, t)):
+#     print("{}".format(get_order_list(n, t, d)))
 
-print("{}".format(getCSP(n, p, t, 0).to_string()))
+constant = getCSP(n, p, t, 0)
+print("{}".format(constant.to_string()))
+getOdds(constant)
